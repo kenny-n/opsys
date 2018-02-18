b63248d577e8f11c8808c2421ed8862161486397
kenny <knnyngo@gmail.com>
2018-02-18 11:17:12 -0800

    final

--- adventuregame/ngoken.adventure.c
+++ adventuregame/ngoken.adventure.c
@@ -1,7 +1,307 @@
-while (currentRoom != endRoom) {
-    getUserInput();
-    if (input == "time")
-        writeTime();
-    else
-    //change currentRoom or prompt user again
+/* 
+ * Kenny Ngo 
+ * Winter 2018
+ * CS 344
+ * Program 2
+ * adventure
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <fcntl.h>
+
+/*global*/
+pthread_mutex_t mutex;
+char* startName; /* room name of the start room */
+char* roomName;  /* current room name */
+char* roomType;  /* type of current room */
+char* folder;    /* latest working directory name */
+
+/* grab the latest rooms directory */
+void getDir()
+{
+	char* resultD;
+	resultD = malloc(sizeof(char) * 128);
+	DIR* d;
+	d = opendir("."); /* checking within current directory */
+
+	struct dirent* dp;
+	struct stat* stats;
+	time_t tiempo; /* will store the directory's mod time */
+	int new = 0; /* the pivot for checking which directory is newer */
+
+	stats = (struct stat*)malloc(sizeof(struct stat));
+	dp = malloc(sizeof(struct dirent));
+
+	if (d != NULL) 
+	{
+		while (dp = readdir(d))
+		{
+			if ((stat(dp->d_name, stats) == 0) && 
+				(S_ISDIR(stats->st_mode) && 
+				(strncmp(dp->d_name, "ngoken.rooms", 12) == 0)))
+			{
+				/* saving the current directory's mod time*/
+				tiempo = stats->st_mtime;
+
+				/* checking current mod time against past mod times */
+				if (tiempo >= new)
+				{
+					/* update last highest mod time with current mod time */
+					new = tiempo;
+
+					/* set result as current directory name */
+					strcpy(resultD, dp->d_name);
+				}
+			}
+		}
+		closedir(d);
+	}
+	/* assign the folder variable the value of the latest directory name */
+	strcpy(folder, resultD);
+}
+
+/* prints the room information */
+void getRooms(char rname[32])
+{
+	char fname[128];
+	sprintf(fname, "./%s/%s", folder, rname);
+	strcat(fname, ".txt");
+	/* building the file name using folder name and rname */
+	FILE* f;
+	f = fopen(fname, "r");
+
+	char line[128];
+	fgets(line, 128, f);
+
+	/* grabbing the room name to print */
+	char* name = strtok(line, " ");
+	name = strtok(NULL, " ");
+	name = strtok(NULL, "\n");
+
+	/* since the start room is not called its room name */
+	/* we have to apply special rules on how to handle its names */
+	if (strcmp(rname, "START_ROOM") == 0)
+		strcpy(startName, name);
+	if (strcmp(roomName, startName) == 1)
+		strcpy(roomName, name);
+
+	/* printing the required information */
+	printf("CURRENT LOCATION: %s\n", name);
+	printf("POSSIBLE CONNECTIONS:");
+	int i;
+	i = 0;
+
+	/* iterate through the lines of the file to get/print connections */
+	while(fgets(line, 128, f) != NULL) 
+	{
+		char* search = strtok(line, " ");
+		if (strcmp(search, "CONNECTION") == 0)
+		{
+			search = strtok(NULL, ":");
+			search = strtok(NULL, "\n");
+			/* using i to print commas after the first connection */
+			if (i > 0)
+				printf(",");
+			printf("%s", search);
+
+		/* also using this opportunity to grab the room type */
+		} else if (strcmp(search, "ROOM") == 0)
+		{
+			search = strtok(NULL, ":");
+			search = strtok(NULL, " \n");
+			strcpy(roomType, search);
+		}
+
+		i++;
+	}
+	printf(".\n"); /* finishing the output line with '.' and '\n'*/
+}
+
+/* reiterates through the file to see if input is a valid connection */
+int validate(char rname[32], char input[32])
+{
+	char fname[128];
+	sprintf(fname, "./%s/%s", folder, rname);
+	strcat(fname, ".txt");
+	/* building the file name using folder name and rname */
+	FILE* f;
+	f = fopen(fname, "r");
+	if (f == NULL)
+		return 0;
+
+	char line[128];
+	fgets(line, 128, f);
+
+	while(fgets(line, 128, f) != NULL) 
+	{
+		char* search = strtok(line, " ");
+
+		/* same code as getRooms but this one returns 1 if input found */
+		if (strcmp(search, "CONNECTION") == 0)
+		{
+			search = strtok(NULL, " ");
+			search = strtok(NULL, " \n");
+			if (strcmp(search, input) == 0){
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+/* write current time to file */
+void* getTime()
+{
+	FILE* f;
+	f = fopen("currentTime.txt", "w+");
+
+	/* strOut to hold the string to output */
+	char* strOut;
+	strOut = malloc(sizeof(char) * 128);
+
+	/* time properties */
+	struct tm* tiempo;
+	time_t now = time(0);
+	tiempo = localtime(&now);
+	
+	/* saving and writing current time to file using */
+	/* the format H:Mam/pm, Day, Month DD YYYY */
+	strftime(strOut, 128, "%I:%M%P, %A, %B %d %G\n", tiempo);
+	fputs(strOut, f);
+	fclose(f);
+}
+
+/* prints the contents of the currentTime.txt file */
+void writeTime()
+{
+	FILE* f;
+	f = fopen("currentTime.txt", "r");
+
+	/* holds the string being read in from file */
+	char* strIn;
+	strIn = malloc(sizeof(char) * 128);
+	
+	fgets(strIn, 128, f);
+	printf("%s\n", strIn);
+	fclose(f);
+}
+
+/* opens new thread and writes the current time to a file using getTime */
+void thread()
+{
+	pthread_t thred; /* initialize the thread */
+	pthread_mutex_init(&mutex, NULL);
+
+	pthread_mutex_lock(&mutex); /* lock thread */
+	
+	/* run getTime to write the time to the file */
+	int tid = pthread_create(&thred, NULL, getTime, NULL);
+	
+	pthread_mutex_unlock(&mutex); /* unlock and remove thread */
+	pthread_mutex_destroy(&mutex);
+}
+
+/* game playing function, stops when at end room */
+void playGame(char current[64])
+{
+	/* initializing values for endgame records */
+	int turns;
+	turns = 0;
+	char* path[128];
+
+	int success; /* flag for game over */
+	success = 0;
+
+	char input[128]; /* stores user input */
+
+	strcpy(roomName, current);
+	getRooms(roomName); /* print the starting room info */
+
+	while (success != 1) 
+	{
+		printf("WHERE TO? >");
+		scanf("%s", input);
+		printf("\n");
+
+		/* user asks for the time */
+		if (strcmp(input, "time") == 0)
+		{
+			thread();
+			writeTime();
+
+		/* validates input */	
+		} else if (!validate(roomName, input))
+		{
+			printf("HUH? I DON’T UNDERSTAND THAT ROOM. TRY AGAIN.\n\n");
+			getRooms(roomName); /* reprint current room info */
+		
+		/* input is valid, continue to input room */
+		} else
+		{
+			path[turns] = malloc(sizeof(char) * 8);;
+			strcpy(path[turns], roomName); /* add to visited rooms */
+			turns++; /* increment turns taken */
+			strcpy(roomName, input); /* set current room to input */
+
+			/* in case the user asked for the start room again */
+			if (strcmp(roomName, startName) == 0)
+				strcpy(roomName, "START_ROOM"); 
+
+			/* print next rooms info */
+			getRooms(roomName);
+		}
+		
+		/* check room type, end game if true */
+		if (strcmp(roomType, "END_ROOM") == 0)
+			success = 1;
+	}
+	/* printing endgame info */
+	printf("\nYOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\n");
+	printf("YOU TOOK %d STEPS. ", turns);
+	printf("YOUR PATH TO VICTORY WAS:\n");
+	
+	int j;
+	/* start at j = 1 because path[0] is start room */
+	for (j = 1; j < turns; j++)
+	{
+		/* check if starting room was revisited, print its name if so */
+		if (strcmp(path[j], "START_ROOM") == 0)
+			printf("%s\n", startName);
+		else
+			printf("%s\n", path[j]);
+	}
+	/* append end room name to path */
+	printf("%s\n", roomName);
+}
+
+/* main function that initializes and starts the game */
+int main(int argc, char* argv[])
+{
+	/* initializing all global variables */
+	folder = malloc(sizeof(char) * 32);
+	roomName = malloc(sizeof(char) * 8);
+	roomType = malloc(sizeof(char) * 16);
+	startName = malloc(sizeof(char) * 16);
+
+	/* because the starting room will be named START_ROOM */
+	/* finding the starting room is not needed and can start immediately */
+	char start[10];
+	strcpy(start, "START_ROOM");
+
+	/* set the current directory to the latest rooms folder */
+	getDir();
+
+	/* begin the game */
+	playGame(start);
+
+	/* for '$ echo $?' */
+	return 0;
 }
--- adventuregame/ngoken.buildrooms.c
+++ adventuregame/ngoken.buildrooms.c
@@ -1,31 +1,48 @@
+/* 
+ * Kenny Ngo 
+ * Winter 2018
+ * CS 344
+ * Program 2
+ * buildrooms
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
+
+/*global*/
+const char* names[] = {"Nehalem", "Westmere", "SndyBrdg", "IvyBridg", "Haswell", 
+		 			   "Broadwll", "Skylake", "KabyLake", "CoffeeLk", "IceLake"};
+
+const char* rTypes[] = {"START_ROOM", "MID_ROOM", "END_ROOM"};
 
 struct room
 {
 	int id;
 	char* name;
-	int roomType; /*0 is START_ROOM, 1 is MID_ROOM, 2 is END_ROOM*/
+	int roomType; /* 0 is START_ROOM, 1 is MID_ROOM, 2 is END_ROOM */
 	int numOutboundConnections;
 	struct room* outboundConnections;
 };
 
 void PrintRoomOutboundConnections(struct room input, char* dir)
 {	
-	/*setting up the file name for the room*/
+	/* setting up the file name for the room */
 	char* fname;
-	fname = malloc(strlen(dir)+1+7+strlen(input.name));
-	const char* str[] = {"./", dir, "/", input.name, ".txt"};
+	fname = malloc(strlen(dir)+1+7+strlen(rTypes[input.roomType]));
+	const char* special[] = {"./", dir, "/", "START_ROOM", ".txt"};
+	const char* normal[] = {"./", dir, "/", input.name, ".txt"};
 	int i;
 	for (i = 0; i < 5; i++)
-		strcat(fname, str[i]);
+		/* if roomType is START_ROOM, name file START_ROOM.txt */
+		if (input.roomType == 0)
+			strcat(fname, special[i]);
+		else
+			strcat(fname, normal[i]);
 
-	/*opening/creating/reporting error for new file*/
+	/* opening/creating/reporting error for new file */
 	FILE *f = fopen(fname, "w");
 	if (f == NULL)
 	{
@@ -33,120 +50,104 @@ void PrintRoomOutboundConnections(struct room input, char* dir)
 		exit(1);
 	}
 
-	/*print room info*/
+	/* print room info */
 	fprintf(f, "ROOM NAME: %s\n", input.name);
 	for (i = 0; i < input.numOutboundConnections; i++)
-	{
 		fprintf(f, "CONNECTION %d: %s\n", i + 1, input.outboundConnections[i].name);
-	}
 
-	/*room type string array*/
-	const char* rTypes[] = {"START_ROOM", "MID_ROOM", "END_ROOM"};
 	fprintf(f, "ROOM TYPE: %s\n", rTypes[input.roomType]);
 
 	fclose(f);
 }
 
-/*Returns true if all rooms have 3 to 6 outbound connections, false otherwise*/ 
+/* returns true if all rooms have 3 to 6 outbound connections, false otherwise */ 
 int IsGraphFull(struct room* rooms)  
 {
-    int i;
-    for (i = 0; i < 7; i++)
-    {
-        if (rooms[i].numOutboundConnections < 3)
-            return 0;
-    }
-    return 1;
+	int i;
+	for (i = 0; i < 7; i++)
+		if (rooms[i].numOutboundConnections < 3)
+			return 0;
+	return 1;
 }
 
-/*Returns a random room, does NOT validate if connection can be added*/ 
+/* returns a random room, does NOT validate if connection can be added */ 
 struct room* GetRandomRoom(struct room* rooms)
 {
-    return &rooms[rand() % 7];
+	return &rooms[rand() % 7];
 }
 
-/*Returns true if a connection can be added from room x (< 6 outbound connections), false otherwise*/ 
+/* returns true if connection can be added to x (< 6 outbound connections), false otherwise */ 
 int CanAddConnectionFrom(struct room x) 
 {
-    return (x.numOutboundConnections < 6);
+	return (x.numOutboundConnections < 6);
 }
 
-/*Returns true if a connection from room x to room y already exists, false otherwise*/ 
+/* returns true if a connection from x to y already exists, false otherwise */ 
 int ConnectionAlreadyExists(struct room x, struct room y)
 {
-    int i;
-    for (i = 0; i < x.numOutboundConnections; i++)
-    {
-        if (x.outboundConnections[i].id == y.id)
-            return 1;
-    }
-    return 0;
+	int i;
+	for (i = 0; i < x.numOutboundConnections; i++)
+		if (x.outboundConnections[i].id == y.id)
+			return 1;
+	return 0;
 }
 
-/*Connects rooms x and y together, does not check if this connection is valid*/ 
+/* connects x to y, does not check if this connection is valid */ 
 void ConnectRoom(struct room* x, struct room* y) 
 {
-    int openDoorIndex;
-    openDoorIndex = x->numOutboundConnections;
-    (*x).outboundConnections[openDoorIndex] = *y;
-    (*x).numOutboundConnections++;
+	int openDoorIndex;
+	openDoorIndex = x->numOutboundConnections;
+	(*x).outboundConnections[openDoorIndex] = *y;
+	(*x).numOutboundConnections++;
 }
 
-/*Returns true if rooms x and y are the same room, false otherwise*/ 
+/* returns true if x and y are the same room, false otherwise */ 
 int IsSameRoom(struct room x, struct room y) 
 {
     return (x.id == y.id);
 }
 
-/*Adds a random, valid outbound connection from a room to another room*/ 
+/* adds a random, valid outbound connection from a room to another room */ 
 void AddRandomConnection(struct room* rooms)
 {
-    struct room* A;  /*Maybe a struct, maybe global arrays of ints*/ 
-    struct room* B;
+	struct room* A;
+	struct room* B;
 
-    while(1)
-    {
-        A = GetRandomRoom(rooms);
+	while(1)
+	{
+		/* get random room */
+		A = GetRandomRoom(rooms);
 
-        if (CanAddConnectionFrom(*A))
-            break;
-    }
+		/* if random room has room for another connection */
+		if (CanAddConnectionFrom(*A))
+			break;
+	}
 
-    do
-    {
-        B = GetRandomRoom(rooms);
-    }
-    while(!CanAddConnectionFrom(*B) || IsSameRoom(*A, *B) || ConnectionAlreadyExists(*A, *B));
+	do
+	{
+		/* get 2nd random room */
+		B = GetRandomRoom(rooms);
+	} while (!CanAddConnectionFrom(*B) || IsSameRoom(*A, *B) || 
+			ConnectionAlreadyExists(*A, *B));
 
-    ConnectRoom(A, B);
-    ConnectRoom(B, A);
-}
-
-void PrintToFile(struct room* rooms)
-{
-
+	/* when the 2nd room fills out the reqs we can connect them */
+	ConnectRoom(A, B);
+	ConnectRoom(B, A);
 }
 
 int main(int argc, char* argv[])
 {
-	/*for random numbers*/
+	/* for random numbers */
 	srand(time(NULL)); 
 
-	/*names of the rooms*/
-	const char* names[] = {"Nehalem", "Westmere", "SndyBrdg", "IvyBridg", "Haswell", 
-			 			   "Broadwll", "Skylake", "KabyLake", "CoffeeLk", "IceLake"};
-	/*room type strings*/
-	const char* rTypes[] = {"START_ROOM", "MID_ROOM", "END_ROOM"};
-
-	/*to check if name or room type is used by another room*/
+	/* to check if name or room type is used by another room */
 	int usedName[10] = {0,0,0,0,0,0,0,0,0,0};
-	int usedType[2] = {0,0}; /*uT[0] is START_ROOM, uT[1] is END_ROOM */
+	int usedType[2] = {0,0}; /* uT[0] is START_ROOM, uT[1] is END_ROOM */
 
-
-	/*array containing the rooms so that they are easy to manipulate*/
+	/* array containing the rooms so that they are easy to manipulate */
 	struct room rooms[7];
 
-	/*initializing values of each room*/
+	/* initializing values of each room */
 	int i;
 	for (i = 0; i < 7; i++)
 	{
@@ -158,36 +159,41 @@ int main(int argc, char* argv[])
 		int randName;
 		randName = rand() % 10;
 
-		while (usedName[randName] == 1) /*finds unused index*/
+		while (usedName[randName] == 1) /* finds unused index */
 			randName = rand() % 10;
-		usedName[randName] = 1; /*used name at this index = true*/
+		usedName[randName] = 1; /* used name at this index = true */
 		strcpy(rooms[i].name, names[randName]);
 
-		/*attempting to assign room types randomly here*/
+		/* attempting to assign room types randomly here */
 		int randType = rand() % 3;
-		if (randType == 0 && usedType[0] == 0) {
+		if (randType == 0 && usedType[0] == 0) 
+		{
 			rooms[i].roomType = 0;
-			usedType[0] = 1; /*used room type START_ROOM*/
-		} else if (randType == 2 && usedType[1] == 0) {
+			usedType[0] = 1; /* used room type START_ROOM */
+		} else if (randType == 2 && usedType[1] == 0) 
+		{
 			rooms[i].roomType = 2;
-			usedType[1] = 1; /*used room type END_ROOM*/
+			usedType[1] = 1; /* used room type END_ROOM */
 		} else {
-			rooms[i].roomType = 1; /*most rooms will be this*/
+			rooms[i].roomType = 1; /* most rooms will be this */
 		}
 		
-		/*this section is used to force START_ROOM or END_ROOM types*/
-		/*if not already assigned by the 2nd to last iteration*/
-		if (i == 5 && (usedType[0] < 1 && usedType[1] < 1)) {
+		/* this section is used to force START_ROOM or END_ROOM types */
+		/* if not already assigned by the 2nd to last iteration */
+		if (i == 5 && (usedType[0] < 1 && usedType[1] < 1)) 
+		{
 			randType = rand() % 2;
-			if (randType == 0) {
+			if (randType == 0) 
+			{
 				rooms[i].roomType = 0;
 				usedType[0] = 1;
 			}
-			else {
+			else 
+			{
 				rooms[i].roomType = 2;
 				usedType[1] = 1;
 			}
-		/*on the final iteration, if one type req not met, then force*/
+		/* on the final iteration, if one type req not met, then force it */
 		} else if (i == 6 && usedType[0] < 1) {
 			rooms[i].roomType = 0;
 		} else if (i == 6 && usedType[1] < 1) {
@@ -195,13 +201,11 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	/*Create all connections in graph*/ 
+	/* create all connections in graph */ 
 	while (!IsGraphFull(rooms))
-	{
 	    AddRandomConnection(rooms);
-	}
-
-	/*creating the directory name*/
+	
+	/* creating the directory name */
 	char prefix[strlen("ngoken.rooms.")];
 	strcpy(prefix, "ngoken.rooms.");
 	int pid;
@@ -209,20 +213,18 @@ int main(int argc, char* argv[])
 	char dir[strlen(prefix)+1+4];
 	snprintf(dir, strlen(prefix)+1+5, "%s%d", prefix, pid);
 
-	/*making the directing*/
+	/* making the directory */
 	int result;
 	result = mkdir(dir, 0755);
 
-	/*exit if error on making directory*/
+	/* exit if error on making directory */
 	if (result > 0)
 	{
 		printf("error mking dir\n");
 		exit(1);
 	}
-
+	/* printing room info to files in dir */
 	int j;
 	for (j = 0; j < 7; j++)
-	{
 		PrintRoomOutboundConnections(rooms[j], dir);
-	}
 }
